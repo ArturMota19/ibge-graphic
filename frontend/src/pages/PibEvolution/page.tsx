77c904228961e3736686d010197c5d3dad887ea3
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-09-16 15:57:11 -0300

    FEAT: Fetch right ibge api

--- frontend/src/pages/PibEvolution/page.tsx
+++ frontend/src/pages/PibEvolution/page.tsx
@@ -7,18 +7,19 @@ import { useEffect, useState } from "react";
 // Styles
 import s from "./page.module.css";
 import { BaseRequest } from "../../services/BaseRequest";
-import Spinner from "../../components/basics/SpinnerLoading/SpinnerLoading";
 
 export default function PibEvolution() {
   const [isLoading, setIsLoading] = useState(false)
 
   useEffect(() => {
     async function FetchIBGE(){
+      // query builder: https://servicodados.ibge.gov.br/api/docs/agregados?versao=3#api-bq
       const response = await BaseRequest({
         method: "GET",
-        url: "https://servicodados.ibge.gov.br/api/v3/agregados/1705/variaveis?localidades=N7",
+        url: "https://servicodados.ibge.gov.br/api/v3/agregados/6784/periodos/1996|1997|1998|1999|2000|2001|2002|2003|2004|2005|2006|2007|2008|2009|2010|2011|2012|2013|2014|2015|2016|2017|2018|2019|2020|2021|2022/variaveis/9808|9812?localidades=N1[all]",
         setIsLoading
       })
+      console.log(response)
     }
     FetchIBGE()
   },[])
