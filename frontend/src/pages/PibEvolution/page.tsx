11ba71c0c7bc7d7cc9816feda62904de68f6d974
ArturMota19 <artur19rodriguesmota@gmail.com>
2025-09-17 09:04:21 -0300

    FEAT: Add input to select graph data

--- frontend/src/pages/PibEvolution/page.tsx
+++ frontend/src/pages/PibEvolution/page.tsx
@@ -4,13 +4,45 @@
 import { useEffect, useState } from "react";
 // Imports
 import { LineChart, Line, CartesianGrid, XAxis, YAxis, Legend } from "recharts";
+import { MultiSelect } from "primereact/multiselect";
 // Styles
 import s from "./page.module.css";
 import { BaseRequest } from "../../services/BaseRequest";
+import Button from "../../components/basics/Button/Button";
 
 export default function PibEvolution() {
     const [isLoading, setIsLoading] = useState(false);
-    const [data, setData] = useState([])
+    const [data, setData] = useState([]);
+    const [selectedYears, setSelectedYears] = useState([]);
+    const years = [
+        { label: "Ano 1996", value: "1996" },
+        { label: "Ano 1997", value: "1997" },
+        { label: "Ano 1998", value: "1998" },
+        { label: "Ano 1999", value: "1999" },
+        { label: "Ano 2000", value: "2000" },
+        { label: "Ano 2001", value: "2001" },
+        { label: "Ano 2002", value: "2002" },
+        { label: "Ano 2003", value: "2003" },
+        { label: "Ano 2004", value: "2004" },
+        { label: "Ano 2005", value: "2005" },
+        { label: "Ano 2006", value: "2006" },
+        { label: "Ano 2007", value: "2007" },
+        { label: "Ano 2008", value: "2008" },
+        { label: "Ano 2009", value: "2009" },
+        { label: "Ano 2010", value: "2010" },
+        { label: "Ano 2011", value: "2011" },
+        { label: "Ano 2012", value: "2012" },
+        { label: "Ano 2013", value: "2013" },
+        { label: "Ano 2014", value: "2014" },
+        { label: "Ano 2015", value: "2015" },
+        { label: "Ano 2016", value: "2016" },
+        { label: "Ano 2017", value: "2017" },
+        { label: "Ano 2018", value: "2018" },
+        { label: "Ano 2019", value: "2019" },
+        { label: "Ano 2020", value: "2020" },
+        { label: "Ano 2021", value: "2021" },
+        { label: "Ano 2022", value: "2022" },
+    ];
     // const data = [{ name: "Page A", uv: 400, pv: 2400, amt: 2400 }];
 
     useEffect(() => {
@@ -26,20 +58,43 @@ export default function PibEvolution() {
         FetchIBGE();
     }, []);
 
-    const MyChart = () => (
-        <LineChart width={600} height={300} data={data}>
-            <CartesianGrid />
-            <Line dataKey="uv" />
-            <XAxis dataKey="name" />
-            <YAxis />
-            <Legend />
-        </LineChart>
-    );
+    const MyChart = () => {
+        if (data.length == 0) return null;
+        return (
+            <LineChart width={600} height={300} data={data}>
+                <CartesianGrid />
+                <Line dataKey="uv" />
+                <XAxis dataKey="name" />
+                <YAxis />
+                <Legend />
+            </LineChart>
+        );
+    };
 
     return (
         <main className={s.wrapperMain}>
             <section className={s.wrapperInterLogo}>
-                <h1>Evolução do PIB</h1>
+                <div className={s.wrapperHeaderDiv}>
+                    <h1>Evolução do PIB</h1>
+                    <form className={s.wrapperForm}>
+                        <label htmlFor="years">
+                            Selecione o(s) Ano(s) para análise
+                        </label>
+                        <MultiSelect
+                            value={selectedYears}
+                            onChange={(e) => setSelectedYears(e.value)}
+                            options={years}
+                            name="years"
+                            placeholder="Anos para consulta"
+                            className={`customMultiSelect`}
+                        />
+                        <Button
+                            text="Gerar gráfico"
+                            backgroundColor="#1BB17A"
+                            color="#FFF"
+                        />
+                    </form>
+                </div>
                 <MyChart />
             </section>
         </main>
